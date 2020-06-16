a522ca96a60bc2a37545ff4e4df9a0c2c059a5e8
Kaichao Sun <kaichaosuna@gmail.com>
2020-06-16 12:20:39 +0800

    create api

---
+++ README.md
@@ -0,0 +1,8 @@
+# Substrate API Inventory
+
+## Developement
+
+```shell
+git clone git@github.com:kaichaosun/substrate-api-inventory.git
+
+```
---
+++ src/api/create-instance.js
@@ -0,0 +1,16 @@
+// Import
+const { ApiPromise, WsProvider } = require('@polkadot/api');
+
+async function main() {
+    // Construct
+    const wsProvider = new WsProvider('wss://kusama-rpc.polkadot.io');
+    const api = await ApiPromise.create({ provider: wsProvider });
+
+    // Do something
+    console.log("genesis hash: ", api.genesisHash.toHex());
+    console.log("runtime metadata: ", api.runtimeMetadata.toString());
+    console.log("runtime version: ", api.runtimeVersion.toString());
+    console.log("library info: ", api.libraryInfo);
+}
+
+main();

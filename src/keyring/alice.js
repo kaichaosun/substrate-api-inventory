5648ad9868b5b3d15b2cda14ac0b2ee4d1cd4266
Kaichao Sun <kaichaosuna@gmail.com>
2020-06-16 12:29:56 +0800

    keyring construct

---
+++ src/keyring/alice.js
@@ -0,0 +1,22 @@
+// Import
+const { WsProvider, ApiPromise, Keyring } = require('@polkadot/api');
+
+async function main() {
+    // Construct
+    const wsProvider = new WsProvider('wss://kusama-rpc.polkadot.io');
+    const api = await ApiPromise.create({ provider: wsProvider });
+
+    // Do something
+    console.log("genesis hash: ", api.genesisHash.toHex());
+    // Create a keyring instance
+    const keyring = new Keyring({ type: 'sr25519' });
+
+    // (Advanced, development-only) add with an implied dev seed and hard derivation
+    const alice = keyring.addFromUri('//Alice', { name: 'Alice default' });
+
+    // Log some info
+    console.log(`${alice.meta.name}: has address ${alice.address} with publicKey [${alice.publicKey}]`);
+
+}
+
+main();

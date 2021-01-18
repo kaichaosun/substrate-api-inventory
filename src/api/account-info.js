27c9e98d1ec10183848284be6559dc15fa07c73a
Kaichao Sun <kaichaosuna@gmail.com>
2021-01-18 17:01:55 +0800

    update api

--- src/api/account-info.js
+++ src/api/account-info.js
@@ -3,7 +3,7 @@ const { ApiPromise, WsProvider } = require('@polkadot/api');
 
 async function main() {
     // Construct
-    const wsProvider = new WsProvider('wss://cc3-5.kusama.network');
+    const wsProvider = new WsProvider('wss://rpc.polkadot.io');
     const api = await ApiPromise.create({ provider: wsProvider });
 
     const result = await api.query.system.account("EajfcfBT8ca7pU3QAQZB6UJmpS4TKs2aaG8yGyUU4PiTUnE");
